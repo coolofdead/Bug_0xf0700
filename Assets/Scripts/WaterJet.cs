f28a713953e5e28f91832d5f288abae20471df24
Jojo <jordangrilly@gmail.com>
2023-06-10 16:49:05 +0200

    Update : Water jet in progress

---
+++ Assets/Scripts/WaterJet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+
+
+public class WaterJet : MonoBehaviour
+{
+    ObiEmitter emitter;
+    public float emissionSpeed = 10;
+    void Start()
+    {
+        emitter = GetComponentInChildren<ObiEmitter>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            emitter.speed = emissionSpeed;
+        }
+        else
+        {
+            emitter.speed = 0;
+        }
+    }
+}
