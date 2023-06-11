61d8fe12829b5c32f5f8c0eea7b7d16f797e1a86
Jojo <jordangrilly@gmail.com>
2023-06-11 15:46:17 +0200

    Update : Extinguish fire on click

--- Assets/Scripts/Extinguisher.cs
+++ Assets/Scripts/Extinguisher.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Extinguisher : MonoBehaviour
 {
+    [SerializeField] private float amountExtinguishedPerSecond = 1f;
     void Start()
     {
         
@@ -11,17 +12,20 @@ public class Extinguisher : MonoBehaviour
 
     void Update()
     {
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 10f))
+        if (!Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 10f))
         {
-            Debug.Log(hit.collider.name);
-            if (hit.collider.gameObject.tag == "Fire")
-            {
-              
-                if (Input.GetMouseButtonDown(0))
-                {
-                    
-                }
-            }
+            return;
+        }
+
+        if (!hit.collider.TryGetComponent(out Fire fire))
+        {
+            return;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Extinguishing !");
+            fire.TryExtinguish(amountExtinguishedPerSecond * Time.deltaTime);
         }
     }
 }
