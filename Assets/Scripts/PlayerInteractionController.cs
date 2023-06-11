a7173cc9b6657eb952aac109f76994a58ff28f39
Jojo <jordangrilly@gmail.com>
2023-06-11 15:57:36 +0200

    Update : Add getter for objectPicked

--- Assets/Scripts/PlayerInteractionController.cs
+++ Assets/Scripts/PlayerInteractionController.cs
@@ -99,4 +99,9 @@ public class PlayerInteractionController : MonoBehaviour
         objectPicked.Release();
         objectPicked = null;
     }
+
+    public ObjectPickable GetPickedObject()
+    {
+        return objectPicked;
+    }
 }
