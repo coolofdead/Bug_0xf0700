9d5a2c92ecc0d9a46b2c0ff9cca74e6b87b1217d
Jojo <jordangrilly@gmail.com>
2023-06-17 16:10:41 +0200

    Update : Add building

---
+++ Assets/Packages/OpenOfficeBuildings/OOB_1/Script/DoorMechanism.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMechanism : MonoBehaviour {
+
+	public GameObject Door;
+
+	public Vector3 doorPosOpen, doorPosClose;
+
+	public float rotSpeed = 1f;
+
+	public bool doorMechBool;
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		if (doorMechBool)
+			Door.transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (doorPosOpen), rotSpeed * Time.deltaTime);
+		else 
+			Door.transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (doorPosClose), rotSpeed * Time.deltaTime);
+	}
+}
