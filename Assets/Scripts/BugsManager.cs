6e48e0c63b812f52541f58d71e0c10f4882f47e7
Jojo <jordangrilly@gmail.com>
2023-06-19 16:51:28 +0200

    Update : Add computer UI

--- Assets/Scripts/BugsManager.cs
+++ Assets/Scripts/BugsManager.cs
@@ -13,36 +13,36 @@ public class BugsManager : MonoBehaviour
 
     private Computer[] computers;
 
-    private void Awake()
-    {
-        computers = FindObjectsOfType<Computer>();
-        Computer.onComputerBugResolve += OnComputerBugResolved;
+    //private void Awake()
+    //{
+    //    computers = FindObjectsOfType<Computer>();
+    //    Computer.onComputerBugResolve += OnComputerBugResolved;
 
-        InitBugs();
-    }
+    //    InitBugs();
+    //}
 
-    private void InitBugs()
-    {
-        var rnd = new System.Random();
-        computers.ToList().Sort((i, c) => rnd.Next());
-        for (int i = 0; i < totalComputersBugged; i++)
-        {
-            computers[i].CreateBug();
-        }
-    }
+    //private void InitBugs()
+    //{
+    //    var rnd = new System.Random();
+    //    computers.ToList().Sort((i, c) => rnd.Next());
+    //    for (int i = 0; i < totalComputersBugged; i++)
+    //    {
+    //        computers[i].CreateBug();
+    //    }
+    //}
 
-    private void OnComputerBugResolved()
-    {
-        totalComputersBugFixed++;
-        totalComputersFixedTMP.text = $"{totalComputersBugFixed} / {totalComputersBugged}";
+    //private void OnComputerBugResolved()
+    //{
+    //    totalComputersBugFixed++;
+    //    totalComputersFixedTMP.text = $"{totalComputersBugFixed} / {totalComputersBugged}";
 
-        if (totalComputersBugFixed != totalComputersBugged) return;
+    //    if (totalComputersBugFixed != totalComputersBugged) return;
 
-        print("all bugs resolved");
-    }
+    //    print("all bugs resolved");
+    //}
 
-    private void OnDestroy()
-    {
-        Computer.onComputerBugResolve -= OnComputerBugResolved;
-    }
+    //private void OnDestroy()
+    //{
+    //    Computer.onComputerBugResolve -= OnComputerBugResolved;
+    //}
 }
