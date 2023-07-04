7cf1766ae3bdacd0f229ec7b1003f4aaf965fffd
Jojo <jordangrilly@gmail.com>
2023-07-04 22:58:32 +0200

    Update : Popup Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private UI_PopUp popup;
+
+    private void Start()
+    {
+        StartCoroutine(Pop());
+    }
+    public void PlayGame()
+    {
+        // Loads the next scene in the build order
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        // Quits the game
+        Application.Quit();
+    }
+
+    IEnumerator Pop()
+    {
+        yield return new WaitForSeconds(3f);
+        popup.Pop();
+    }
+}
